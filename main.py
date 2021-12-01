3789c138de2a6cdf70319f732d083a93edc522cc
Robomaktod <82158768+Robomaktod@users.noreply.github.com>
2021-12-01 09:19:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ while not name:
     name = input("Please write your name correct").strip()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = "localhost"  # "127.0.1.1"
-port = 8000
+port = 2221
 client.connect((host, port))
 
 
@@ -17,17 +17,20 @@ def thread_sending():
         ncode = input('encode msg? \nyes \nno \n')
         if ncode == 'no':
             message_with_name = "n" + '1' + name + " : " + messagesd
+            selector = "first"
             client.send(message_with_name.encode())
+            client.send(selector.encode())
         elif ncode == 'yes':
             message_with_name = name + " : " + messagesd
+            selector = "first"
             client.send(myencode('x' + '1' + message_with_name, 1).encode())
-        else:
-            print("ERROR")
+            client.send(selector.encode())
 
 
 def thread_receiving():
     while True:
         message = client.recv(1024).decode()
+        selector = client.recv(2048).decode()
         if message[0] == 'y':
             print(myencode(message, -1)[2:])
         elif message[0] == 'n':
