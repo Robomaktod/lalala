2d4f7e1becc86d5d8bee113453d5d965c5f615b8
Robomaktod <82158768+Robomaktod@users.noreply.github.com>
2021-12-05 13:38:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,38 +6,36 @@ name = input("Choose your name : ").strip()
 while not name:
     name = input("Please write your name correct").strip()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "localhost"  # "127.0.1.1"
-port = 2221
+host = "192.168.0.107"  # "127.0.1.1"
+port = 1013
 client.connect((host, port))
+client_num = '1'
 
-
-def thread_sending():
+def sending():
     while 1:
         messagesd = input()
         ncode = input('encode msg? \nyes \nno \n')
         if ncode == 'no':
             message_with_name = "n" + '1' + name + " : " + messagesd
-            selector = "first"
             client.send(message_with_name.encode())
-            client.send(selector.encode())
         elif ncode == 'yes':
             message_with_name = name + " : " + messagesd
-            selector = "first"
             client.send(myencode('x' + '1' + message_with_name, 1).encode())
-            client.send(selector.encode())
+        else:
+            print("ERROR")
 
 
-def thread_receiving():
-    while True:
+def msg():
+    while 1:
         message = client.recv(1024).decode()
-        selector = client.recv(2048).decode()
-        if message[0] == 'y':
-            print(myencode(message, -1)[2:])
-        elif message[0] == 'n':
-            print(message[2:])
+        if client_num != message[1]:
+            if message[0] == 'y':
+                print(myencode(message, -1)[2:])
+            elif message[0] == 'n':
+                print(message[2:])
 
 
-thread_send = threading.Thread(target=thread_sending)
-thread_receive = threading.Thread(target=thread_receiving)
+thread_send = threading.Thread(target=sending)
+thread_receive = threading.Thread(target=msg)
 thread_send.start()
 thread_receive.start()
